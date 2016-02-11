7ef80dbea89674f6e0fb3bba433582d34d0e1ece
Joseph Lago <josephlago90@gmail.com>
2016-02-11 14:05:34 -0700

    mini paint mk1

--- app.js
+++ app.js
@@ -1,40 +1,108 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
+	var box = $('.box');
+	var colors = 'red blue green yellow white';
+
+	box.on('click', function() {
+		$(this).addClass(colors)
 	})
 
-	$(".box").on('dblclick', function(){
-		$(this).removeClass(color);
+	box.on('dblclick', function() {
+		$(this).removeClass(colors);
 	})
 
-	$('#reset').on('click', function(){
-		$('.box').removeClass('red blue green yellow white');
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
 	})
 
-	$('#red').on('click', function(){
+	var color = 'white';
+
+	$('#red').on('mousedown', function () {
 		color = 'red';
 	})
 
-	$('#blue').on('click', function(){
+	$('#blue').on('click', function () {
 		color = 'blue';
 	})
 
-	$('#green').on('click', function(){
+	$('#green').on('click', function () {
 		color = 'green';
 	})
 
-	$('#yellow').on('click', function(){
+	$('#yellow').on('click', function () {
 		color = 'yellow';
 	})
 
-	$('#white').on('click', function(){
+	$('#white').on('click', function () {
 		color = 'white';
 	})
 
-
-
-
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//
+// 	var color = "white";
+// 		//selector //listener //callback
+// 	$('.box').on("click", function(){
+// 		$(this).addClass(color); // function body
+// 	})
+//
+// 	$(".box").on('dblclick', function(){
+// 		$(this).removeClass(color);
+// 	})
+//
+// 	$('#reset').on('click', function(){
+// 		$('.box').removeClass('red blue green yellow white');
+// 	})
+//
+// 	$('#red').on('click', function(){
+// 		color = 'red';
+// 	})
+//
+// 	$('#blue').on('click', function(){
+// 		color = 'blue';
+// 	})
+//
+// 	$('#green').on('click', function(){
+// 		color = 'green';
+// 	})
+//
+// 	$('#yellow').on('click', function(){
+// 		color = 'yellow';
+// 	})
+//
+// 	$('#white').on('click', function(){
+// 		color = 'white';
+// 	})
+//
+//
+//
+//
+// })
